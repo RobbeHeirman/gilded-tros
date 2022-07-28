06ab7cf46dedb72ef46e7c44db05e6f808c4549f
robbe <rheirman@overseas.be>
2022-07-28 21:55:31 +0200

    What we want to test written in test_gilded_tros.py

--- Python/test_gilded_tros.py
+++ Python/test_gilded_tros.py
@@ -5,12 +5,25 @@ from gilded_tros import Item, GildedTros
 
 
 class GildedTrosTest(unittest.TestCase):
+    # TODO: Fix existing test. What part of the code does this test? Constructor or update quality?
     def test_foo(self):
         items = [Item("foo", 0, 0)]
         gilded_tros = GildedTros(items)
         gilded_tros.update_quality()
         self.assertEquals("fixme", items[0].name)
 
+
+# TODO: Item constructor test (Pre conditions).
+# TODO: Item constructor happy day
+# TODO: Constructor boundary tests.
+# TODO: test raise ValuError quality UPPER_BOUNDARY > item > LOWER_BOUNDARY
+# TODO: test raise ValueError legendary item != 80
+
+# TODO: update_quality tests
+# TODO: Happy day scenario -> check end result after x loops for each kind of item
+# TODO: check invariants during update_quality function
+#       - item boundaries
+#       - legendary item
 
 if __name__ == '__main__':
     unittest.main()
